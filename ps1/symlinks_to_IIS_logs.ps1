d10314793febc6f93e2022646519726d8b96a351
Maxim Muzafarov <m.muzafarov@gmail.com>
2015-11-16 15:45:37 +0300

    cycle

--- ps1/symlinks_to_IIS_logs.ps1
+++ ps1/symlinks_to_IIS_logs.ps1
@@ -1,12 +1,14 @@
-$computer = "example.domain"
-$sites = [adsi]"IIS://$computer/W3SVC"
-$children = $sites.children 
-ForEach ($child in $children) { 
-    If ($child.KeyType -eq "IIsWebServer") { 
-        $name = $child.AppPoolId.ToString()
-        $id = $child.Name.ToString()
-        $directory = "\\$computer\c`$\inetpub\logs\LogFiles\W3SVC$id"
-        New-Item -ItemType directory -ErrorAction Ignore -Path $name\IIS
-        Invoke-Expression "cmd /c mklink /D $name\IIS\$computer $directory"
-    }                                                             
+$computers = "example1", "example2", "example3"
+ForEach ($computer in $computers) {
+    $sites = [adsi]"IIS://$computer/W3SVC"
+    $children = $sites.children 
+    ForEach ($child in $children) { 
+        If ($child.KeyType -eq "IIsWebServer") { 
+            $name = $child.AppPoolId.ToString()
+            $id = $child.Name.ToString()
+            $directory = "\\$computer\c`$\inetpub\logs\LogFiles\W3SVC$id"
+            New-Item -ItemType directory -ErrorAction Ignore -Path $name\IIS
+            Invoke-Expression "cmd /c mklink /D $name\IIS\$computer $directory"
+        }                                                             
+    }
 }
